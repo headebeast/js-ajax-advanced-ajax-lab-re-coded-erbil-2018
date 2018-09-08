b57efb73bc1a3c526461983418b919105823b387
Hedi Sabah <headesabah@gmail.com>
2018-09-08 11:06:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function getRepositories() {
+  const uri = "https://api.github.com/users/AhmadAbdulkhaliq/repos"
+  const xhr = new XMLHttpRequest()
+  xhr.addEventListener("load", displayRepositories)
+  xhr.open("GET", uri)
+  xhr.send()
+  return false;
+}
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText)
+  const repoList = "<ul>" + repos.map(repo => {
+    const dataUsername = 'data-username="' + repo.owner.login + '"'
+    const dataRepoName = 'data-repository="' + repo.name + '"'
+    return(`
+          <li>
+            <h2>${repo.name}</h2>
+            <a href="${repo.html_url}">${repo.html_url}</a><br>
+            </li>
+          </li>`
+          )
+  }).join('') + "</ul>";
+  document.getElementById("repositories").innerHTML = repoList
+}
